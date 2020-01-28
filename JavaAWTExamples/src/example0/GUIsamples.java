76781d03e8dfea3fc4c9ca02e49601ded6195ca2
qhu <qiruihu@gmail.com>
2020-01-28 12:40:58 -0800

    1/28 update gui example

--- JavaAWTExamples/src/example0/GUIsamples.java
+++ JavaAWTExamples/src/example0/GUIsamples.java
@@ -8,12 +8,32 @@ package example0;
 
 
 import java.awt.Frame;
+import java.awt.Panel;
+import java.awt.Label;
+import java.awt.Button;
+import java.awt.TextField;
+import java.awt.Choice;
+import java.awt.Checkbox;
+import java.awt.CheckboxGroup;
+
 
 public class GUIsamples extends Frame{
 	
-	
+	private Frame mainFrame;
+	private Panel mainPanel;
+
+	private Label label1;
+	private Label label2;
+	private TextField textInput1;
+	private Choice dropdownMenu1;
+	private Checkbox option1;
+	private Checkbox option2;
+	private CheckboxGroup options1;
+
 	public GUIsamples () {
 		
+		
+
 	}
 	
 	
